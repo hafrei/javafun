5043f822666bd3d9fb52806b690859f68c2616e0
hafrei <h.a.freiburger@gmail.com>
2015-02-26 19:04:55 -0500

    Adding Enums.java, hopefully to the right repo

---
+++ Enums.java
@@ -0,0 +1,9 @@
+public class Enums{
+
+    public enum Tester {WHO, REALLY, FRIGGEN, CARES}
+
+    public static void main(String[] args) {
+        Tester tester = Tester.WHO;
+        System.out.println(tester);
+    }
+}
